57a9d3f30e7dc653fc46ca54532aec46dda8fe54
Vatsal Bhatt <Bhattvatsal.J@ad.infosys.com>
2018-03-20 17:40:04 +0100

    test commit

--- fibonacci.c
+++ fibonacci.c
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 // implied. See the License for the specific language governing
 // permissns and limitations under the License.
-//Hello
+//
 #include "fibonacci.h"
 
 unsigned int fibonacci(unsigned int n)
